3646be3846d059035d003233c3974f9f699a7e05
Marco Alemão <marco_alemao@hotmail.com>
2025-05-10 14:03:48 +0200

    add x4

--- src/napoleon/launch/main_launch.py
+++ src/napoleon/launch/main_launch.py
@@ -2,9 +2,15 @@
 import os
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, LogInfo
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, LogInfo, TimerAction
 from launch.substitutions import LaunchConfiguration, TextSubstitution
 from ament_index_python.packages import get_package_share_directory
+import os
+
+# Set environment variables for Gazebo resource paths
+os.environ["GZ_SIM_RESOURCE_PATH"] = "/workspace/install/napoleon/share/napoleon/models"
+os.environ["GAZEBO_MODEL_PATH"] = "/workspace/install/napoleon/share/napoleon/models"
+
 
 
 def generate_launch_description():
@@ -34,8 +40,24 @@ def generate_launch_description():
         }
     )
 
+    # Bridge cmd_vel from ROS2 → Ignition
+    ros_ign_bridge = ExecuteProcess(
+        cmd=[
+            "ros2", "run", "ros_ign_bridge", "parameter_bridge",
+            "/model/tugbot/cmd_vel@geometry_msgs/msg/Twist@ignition.msgs.Twist"
+        ],
+        output="screen"
+    )
+
+    # Delay bridge startup to avoid race conditions
+    delayed_bridge = TimerAction(
+        period=3.0,  # seconds
+        actions=[ros_ign_bridge]
+    )
+
     return LaunchDescription([
         declared_world,
         log_model_path,
-        gz_sim
+        gz_sim,
+        #delayed_bridge
     ])
